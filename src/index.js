fad522aca0b23175d82ed8b0687d57a706dbfc32
Vlad Listgarten <listgarten100@gmail.com>
2021-01-21 20:22:56 +0200

    feat: create basic structure of functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 // typical-arrays-problem
 exports.min = function min (array) {
-  return 0;
-}
+  if(array && array.length !== 0) {
+  let res;
+  res = Math.min.apply(null, array);
+  return res;
+  } else {
+    return 0;
+  }
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if(array && array.length !== 0) {
+  let res;
+  res = Math.max.apply(null, array);
+  return res;
+  } else {
+    return 0;
+  }
+};
 
 exports.avg = function avg (array) {
+  if(array && array.length !== 0) {
+  let sumOfNumbers = null;
+  let res;
+  array.forEach(element => {
+    sumOfNumbers += element; 
+    return sumOfNumbers;
+  });
+  res = sumOfNumbers/array.length;
+  return res;
+} else {
   return 0;
 }
+};
