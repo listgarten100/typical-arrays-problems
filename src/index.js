47399c1741c12bd953a690b048bdba43f358615d
Vlad Listgarten <listgarten100@gmail.com>
2021-01-21 20:27:59 +0200

    refactor: apply formatting

--- src/index.js
+++ src/index.js
@@ -1,35 +1,36 @@
 // typical-arrays-problem
 exports.min = function min (array) {
-  if(array && array.length !== 0) {
   let res;
+
+  if(array && array.length !== 0) {
   res = Math.min.apply(null, array);
   return res;
-  } else {
-    return 0;
-  }
+  
+  } else return 0;
 };
 
 exports.max = function max (array) {
-  if(array && array.length !== 0) {
   let res;
+
+  if(array && array.length !== 0) {
   res = Math.max.apply(null, array);
   return res;
-  } else {
-    return 0;
-  }
+
+  } else return 0;
 };
 
 exports.avg = function avg (array) {
-  if(array && array.length !== 0) {
   let sumOfNumbers = null;
   let res;
+
+  if(array && array.length !== 0) {
   array.forEach(element => {
     sumOfNumbers += element; 
     return sumOfNumbers;
   });
+
   res = sumOfNumbers/array.length;
   return res;
-} else {
-  return 0;
-}
+
+} else return 0;
 };
